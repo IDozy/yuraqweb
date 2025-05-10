c784bad51523eb30826c591a547f77a28cb14d9e
gsoonc <109822183+gsoonc@users.noreply.github.com>
2025-05-10 14:07:25 -0500

    arreglando texto de SecCard

--- src/components/SecurityOcu/SecCard.jsx
+++ src/components/SecurityOcu/SecCard.jsx
@@ -12,10 +12,10 @@ const SecCard = () => {
     >
       {/* Texto a la izquierda */}
       <div className="card-content">
-        <h2>Promovemos la Concienca Resposnable "</h2>
+        <h2>Promovemos la conciencia Operacional Responsable</h2>
         <p>
-          Como parte de nuestro compromiso en seguridad, promovemos las charlass supervisadas procurando mantener al transition a 
-          nuestro personal con respecto al manejo de equipos crítocos en todo tipo de operaciones.
+          Como parte de nuestro compromiso en seguridad, promovemos las charlas supervisadas procurando mantener en constante capacitación a 
+          nuestro personal con respecto al manejo de equipos críticos para su uso en todo tipo de operaciones.
           
         </p>
       </div>
